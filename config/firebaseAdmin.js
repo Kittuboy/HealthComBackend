2a57bab2f68534e6e4e737bad0363dceeee6e746
Kittu Boy <boykittu926@gmail.com>
2026-08-14 17:08:02 +0530

    Update firebaseAdmin.js

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -7,22 +7,29 @@ const {
     getMessaging
 } = require("firebase-admin/messaging");
 
-const serviceAccount =
-    require("../firebase-service-account.json");
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+        : undefined
+};
 
+if (
+    !serviceAccount.projectId ||
+    !serviceAccount.clientEmail ||
+    !serviceAccount.privateKey
+) {
+    throw new Error(
+        "Firebase environment variables are missing"
+    );
+}
 
-const firebaseApp =
-    initializeApp({
+const firebaseApp = initializeApp({
+    credential: cert(serviceAccount)
+});
 
-        credential:
-            cert(serviceAccount)
-
-    });
-
-
-const messaging =
-    getMessaging(firebaseApp);
-
+const messaging = getMessaging(firebaseApp);
 
 module.exports = {
     firebaseApp,
